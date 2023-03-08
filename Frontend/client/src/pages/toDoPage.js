bbed2f9e620142f0c76422e84d182954431b1da2
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-03-08 21:44:13 +0100

    Start creating the Frontend

---
+++ Frontend/client/src/pages/toDoPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function toDoPage() {
+  return (
+    <div>
+      Todos Page, this is where you will be able to see all your todos and add
+      new ones as well. and see other users todos as well if you are logged in
+      and friends with them.
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>
+        <h1>Todo 1</h1>
+        <p>Todo 1 description</p>
+      </div>
+    </div>
+  );
+}
+
+export default toDoPage;
